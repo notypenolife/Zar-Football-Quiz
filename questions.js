e5a364b075480e2b069e3b4bb2cebd75778d25db
notypenolife <c.yang@kings.net.nz>
2024-11-05 15:27:06 +1300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which country has won the most FIFA World Cup titles?",
+        answer: "C. Brazil",
+        options: [
+            "A. Italy",
+            "B. Germany",
+            "C. Brazil",
+            "D. Argentina"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "Who is the all-time top scorer in the UEFA Champions League?",
+        answer: "A. Cristiano Ronaldo",
+        options: [
+            "A. Cristiano Ronaldo",
+            "B. Lionel Messi",
+            "C. Robert Lewandowski",
+            "D. Ruud Van Nistlerooy"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which Premier League team is known as 'The Red Devils'?",
+        answer: "A. Manchester United",
+        options: [
+            "A. Manchester United",
+            "B. Arsenal",
+            "C. Liverpool",
+            "D. Manchester City"
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "In which year was the first FIFA World Cup held?",
+        answer: "D. 1930",
+        options: [
+            "A. 1960",
+            "B. 1942",
+            "C. 1956",
+            "D. 1930"
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the maximum number of players allowed on the field for one team during a football match?",
+        answer: "D. 11",
+        options: [
+            "A. 12",
+            "B. 9",
+            "C. 10",
+            "D. 11"
+
+        ]
+    }
+];
